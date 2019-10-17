bd36a15f03a67665a73af1af662cfd78ae95ad3d
InoueTetsuroh <46564927+InoueTetsuroh@users.noreply.github.com>
2019-10-17 23:08:52 +0900

    add-visualstudio-source
    intで使えるlinq
    stringで使えるlinq

---
+++ Display.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LinqLearning
+{
+    partial class Display
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 12);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(776, 426);
+            this.output.TabIndex = 0;
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(535, 447);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(252, 38);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 493);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button OK;
+    }
+}
+
---
+++ Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqLearning
+{
+    public static class Extension
+    {
+        public static string JoinIntoString(this IEnumerable<char> chars)
+        {
+            return new string(chars.ToArray());
+        }
+    }
+}
